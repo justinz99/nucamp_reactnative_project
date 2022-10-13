5105232cc68b8131e3564e0b6c854d2b3c4a097d
Justin Zhao <justinzhao1999@gmail.com>
2022-10-13 11:28:39 -0700

    added gestures, animations, alerts, persist store

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -15,6 +15,7 @@ import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import AboutScreen from "./AboutScreen";
 import ContactScreen from "./ContactScreen";
 import ReservationScreen from "./ReservationScreen";
+import FavoritesScreen from "./FavoritesScreen";
 import logo from '../assets/images/logo.png'
 
 const Drawer = createDrawerNavigator();
@@ -152,6 +153,29 @@ const ReservationNavigator = () => {
     )
 }
 
+const FavoritesNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Favorites'
+                component={FavoritesScreen}
+                options={({navigation}) => ({
+                    title: 'Favorite Campsites',
+                    headerLeft: () => (
+                        <Icon
+                            name='heart'
+                            type="font-awesome"
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    )
+}
 
 const CustomDrawerContent = (props) => (
     <DrawerContentScrollView {...props}>
@@ -207,6 +231,16 @@ const Main = () => {
                         title: 'Reserve Campsite',
                         drawerIcon: ({ color }) => (
                             <Icon name='tree' type='font-awesome' size={24} iconStle={{ width: 24 }} color={color} />
+                        )
+                    }}
+                />
+                <Drawer.Screen
+                    name='Favorites'
+                    component={FavoritesNavigator}
+                    options={{
+                        title: 'My Favorites',
+                        drawerIcon: ({ color }) => (
+                            <Icon name='heart' type='font-awesome' size={24} iconStle={{ width: 24 }} color={color} />
                         )
                     }}
                 />
